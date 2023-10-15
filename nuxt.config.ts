be8054e9de66a248f42a396ec4e8ccde59348e77
iorjunior <juniorcoda@gmail.com>
2023-10-14 22:25:59 -0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules:[
+    '@nuxtjs/tailwindcss',
+  ],
+  app:{
+    head:{
+      title: 'Faturometro FE23',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name:'language', content:'pt-BR'},
+        { name: 'description', content:'Faturometro FE23'},
+        { name: 'robots', content:'all'},
+      ],
+    }
+  }
+})
